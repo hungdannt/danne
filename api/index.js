728f402a7029a38a44ac843eb265e084d38c503f
Dan NT <caucahotran@gmail.com>
2024-11-03 18:42:02 +0700

    fix

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const serverlesswp = require('serverlesswp');
 const { validate } = require('../util/install.js');
 const { setup } = require('../util/directory.js');
 
-export const runtime = 'edge'; // 'nodejs' is the default
+
 // This is where all requests to WordPress are routed through. See vercel.json or netlify.toml for the redirection rules.
 exports.handler = async function (event, context, callback) {
     // Move the /wp directory to /tmp/wp so that it is writeable.
@@ -22,3 +22,6 @@ exports.handler = async function (event, context, callback) {
         return response;
     }
 }
+
+
+
